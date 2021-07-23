51ad64e3b89d1cbff73a22471f4dee7cc161169f
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-07-23 14:46:56 -0300

    cambios

--- Java/Main.java
+++ Java/Main.java
@@ -5,18 +5,18 @@ class Main  {
         System.out.println("Hola mundo");
 
         //Creacion de primer constructor 
-        Car car = new Car();
-        
-        car.license = "AMQ123";
-        car.driver = "Javier Ramirez";
+        Car car = new Car( "AMQ123", new Account("Javier Ramirez", "AND123"));
         car.passenger = 4;
         car.printDataCar();
+        //car.license = "AMQ123";
+        //car.driver = "Javier Ramirez";
+       
 
         //Creacion de segundo constructor
-        Car car2 = new Car();
+        Car car2 = new Car("ARJ450", new Account("Andrea Rodriguez","AND450"));
         
-        car2.license = "ARJ450";
-        car2.driver = "Andrea Rodriguez";
+        //car2.license = "ARJ450";
+        //car2.driver = "Andrea Rodriguez";
         car2.passenger = 3;
         car2.printDataCar();
         //System.out.println("Car License: " + car2.license);
