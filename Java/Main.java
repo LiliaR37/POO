385f831e0a0d142582c9d2e564ab0d1b907f96d5
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-07-23 11:46:21 -0300

    agregando constructores

--- Java/Main.java
+++ Java/Main.java
@@ -4,8 +4,29 @@ class Main  {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
 
+        //Creacion de primer constructor 
+        Car car = new Car();
+        
+        car.license = "AMQ123";
+        car.driver = "Javier Ramirez";
+        car.passenger = 4;
+        car.printDataCar();
+
+        //Creacion de segundo constructor
+        Car car2 = new Car();
+        
+        car2.license = "ARJ450";
+        car2.driver = "Andrea Rodriguez";
+        car2.passenger = 3;
+        car2.printDataCar();
+        //System.out.println("Car License: " + car2.license);
+
+
+
         
     
     }
+
+    
 }
 
