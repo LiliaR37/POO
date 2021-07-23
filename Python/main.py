58f3f5ad9d53c8d202496c42b83bbde79cec1cfd
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-07-23 15:00:59 -0300

    constructor python

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,5 @@
 from car import Car 
+from account import Account
 
 
 
@@ -6,14 +7,11 @@ from car import Car
 if __name__ == "__main__":
     print("Hola Mundo")
 
-    car = Car()
-    car.license = "AM234"
-    car.driver = "Andres Herrera"
 
-    print(vars(car))
+if __name__ == "__main__":
+    print("Hola Mundo")
     
-    car2 = Car()
-    car2.license = "ARJ450"
-    car2.driver = "Andrea Rodriguez"
-    print(vars(car2))
+    car = Car("AMS234", Account("Andres Herrera", "ANDA876"))
+    print(vars(car))
+    print(vars(car.driver))
         
