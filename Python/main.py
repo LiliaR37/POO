c521c4a438ba89a5d00e5deeaefcca5dc1773ae4
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-07-23 11:59:48 -0300

    agregando el constructor en python

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,19 @@
+from car import Car 
+
+
+
+
 if __name__ == "__main__":
     print("Hola Mundo")
+
+    car = Car()
+    car.license = "AM234"
+    car.driver = "Andres Herrera"
+
+    print(vars(car))
+    
+    car2 = Car()
+    car2.license = "ARJ450"
+    car2.driver = "Andrea Rodriguez"
+    print(vars(car2))
+        
