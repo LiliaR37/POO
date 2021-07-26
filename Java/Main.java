afd3ed2ac0ce1a7b218594761bea8cf3ecf0068c
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-07-26 16:09:45 -0300

    agregando driver y user

--- Java/Main.java
+++ Java/Main.java
@@ -5,7 +5,7 @@ class Main  {
         System.out.println("Hola mundo");
 
         //Creacion de primer constructor 
-        Car car = new Car( "AMQ123", new Account("Javier Ramirez", "AND123"));
+        Car car = new Car( "AMQ123", new Account("Javier Ramirez", "10234135","@gmail.com","123"));
         car.passenger = 4;
         car.printDataCar();
         //car.license = "AMQ123";
@@ -13,7 +13,7 @@ class Main  {
        
 
         //Creacion de segundo constructor
-        Car car2 = new Car("ARJ450", new Account("Andrea Rodriguez","AND450"));
+        Car car2 = new Car("ARJ450", new Account("Andrea Ramirez", "10234135","@gmail.com","123"));
         
         //car2.license = "ARJ450";
         //car2.driver = "Andrea Rodriguez";
@@ -21,6 +21,8 @@ class Main  {
         car2.printDataCar();
         //System.out.println("Car License: " + car2.license);
 
+        User user = new User("Pepe", "1234", "gmail.com", "password");
+        user.printDataAccount();
 
 
         
