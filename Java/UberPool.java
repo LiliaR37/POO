860daffb9ce7598178f9f72b26ecaab88553725a
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-07-26 10:53:34 -0300

    herencia

---
+++ Java/UberPool.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class UberPool extends Car {
+    String brand;
+    String model;
+
+    //constructor 
+    public UberPool(String license, Account driver, String brand, String model){
+        super(license, driver);
+        this.brand = brand;
+        this.model = model;
+
+        
+    }
+}
---
+++ Java/UberX.java
@@ -0,0 +1,16 @@
+package Java;
+
+public class UberX extends Car{
+    String brand;
+    String model;
+
+    //constructor 
+    public UberX(String license, Account driver, String brand, String model){
+        super(license, driver);
+        this.brand = brand;
+        this.model = model;
+
+        
+    }
+    
+}
